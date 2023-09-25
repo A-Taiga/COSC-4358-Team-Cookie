6c520cf7c6585593a35c356c0ba3cac076196730
muhaiminbadar <40192639+muhaiminbadar@users.noreply.github.com>
2023-09-25 18:00:09 -0500

    test level

---
+++ The Invaders/Assets/scripts/Player/characterMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    [SerializeField] 
+    protected float xSpeed = 1f, ySpeed = 0.75f;
+    private Vector3 moveDelta;
+    protected virtual void Awake() {
+        blockLayer = LayerMask.GetMask(TagManager.BOUNDARY_TAG);
+        Debug.Log("blockLayer: " + blockLayer);
+    }
+
+    protected virtual void HandleMovement(float x, float y) {
+        moveDelta = new Vector3(x * xSpeed, y * ySpeed, 0f);
+        transform.Translate(moveDelta.x * Time.deltaTime, moveDelta.y * Time.deltaTime, 0f);
+    }
+
+    public Vector3 GetMoveDelta() {
+        return moveDelta;
+    }
+
+}
---
+++ The Invaders/Assets/scripts/Player/playerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : characterMovement
+{
+    private float moveX, moveY;
+
+    private void FixedUpdate() {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+        HandleMovement(moveX, moveY);
+    }
+}
