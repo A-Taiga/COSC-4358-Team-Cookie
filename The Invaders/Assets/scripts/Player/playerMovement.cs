9e7c69386ec6ece09f7354035eadb46c0f754d9a
Anthony Polka <anthonypolka@gmail.com>
2023-10-04 19:34:53 -0500

    added MC animation

--- The Invaders/Assets/scripts/Player/playerMovement.cs
+++ The Invaders/Assets/scripts/Player/playerMovement.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class playerMovement : characterMovement
 {
+    
     private float moveX, moveY;
     private bool run;
 
     private Camera mainCam;
     private Vector2 mousePos;
-    private Vector2 lookDir;
+    private Vector3 lookDir;
     private Vector3 tempScale;
 
-    private Animator anim;
+    public Animator animator;
+    
 
     protected override void Awake() {
         base.Awake();
@@ -22,12 +24,42 @@ public class playerMovement : characterMovement
 
 
     private void FixedUpdate() {
+
+
+        /* reset Speed parameter of animation */
+        animator.SetFloat("Speed",0);
+
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
         run = Input.GetButton("Jump");
         HandleMovement(moveX, moveY, run);
-    }
 
-    void HandlePlayerTurning() {}
+
+
+
+        // Debug.Log("moveX: " + moveX + " moveY: " + moveY);
+
+        if (moveX != 0)
+            animator.SetFloat("Speed", MathF.Abs(moveX));
+
+        if (moveY != 0)
+            animator.SetFloat("Speed", MathF.Abs(moveY));
+        
+        /* handle player direction */
+            HandlePlayerTurning(moveX);
+    }
+    void HandlePlayerTurning(float moveX) 
+    {
+        if(moveX < 0)
+        {
+            lookDir = new Vector3(-0.05f, 0.05f, 0.05f);
+            transform.localScale = lookDir;
+        }
+        else if(moveX > 0)
+        {
+            lookDir = new Vector3(0.05f, 0.05f, 0.05f);
+            transform.localScale = lookDir;
+        }
+    }
     void HandlePlayerAnimation(float x, float y) {}
 }
