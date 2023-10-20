1a88e4209209ea7ae5ba535dfef0c1f0ace97453
Anthony Polka <anthonypolka@gmail.com>
2023-10-20 14:13:05 -0500

    added attack animation for player

--- The Invaders/Assets/scripts/Player/playerMovement.cs
+++ The Invaders/Assets/scripts/Player/playerMovement.cs
@@ -10,10 +10,13 @@ public class playerMovement : characterMovement
 
     private Camera mainCam;
     private Vector2 mousePos;
-    private Vector3 lookDir;
     private Vector3 tempScale;
 
     public Animator animator;
+
+    public Vector3 screenPosition;
+    public Vector3 worldPosition;
+
     
 
     protected override void Awake() {
@@ -23,19 +26,62 @@ public class playerMovement : characterMovement
     }
 
 
-    private void FixedUpdate() {
+    private void Update() {
+
+
+
 
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
         run = Input.GetButton("Jump");
         HandleMovement(moveX, moveY, run);
 
-        Debug.Log("X: " + moveDelta.x + "Y: " + moveDelta.y);
+        // Debug.Log("X: " + moveDelta.x + "Y: " + moveDelta.y);
+
+        /* player movement animation */
         animator.SetFloat("Horizontal", moveDelta.x);
         animator.SetFloat("Vertical", moveDelta.y);
         animator.SetFloat("Speed", moveDelta.sqrMagnitude);
-        
+
+         /* get position of mouse relative to the world */
+        screenPosition = Input.mousePosition;
+        worldPosition = mainCam.ScreenToWorldPoint(screenPosition);
+
+
+        // if(Input.GetMouseButtonDown(0))
+        // {
+        //     Debug.Log("MOUSE CLICK");
+        //     if(GameObject.Find("Player").transform.position.x > worldPosition.x)
+        //     {
+        //         animator.SetBool("AttackLeft", true);
+        //     }
+        // }
+
+
+        //Debug.Log("PLAYER: " + GameObject.Find("Player").transform.position + "MOUSE: " + worldPosition);
+
+
+
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(GameObject.Find("Player").transform.position.x > worldPosition.x)
+            {
+                animator.SetBool("AttackLeft", true);
+            }
+            else
+            {
+                animator.SetBool("AttackRight", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("AttackLeft", false);
+            animator.SetBool("AttackRight", false);
+
+        }
 
     }
     void HandlePlayerAnimation(float x, float y) {}
+
 }
